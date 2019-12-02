3a3805867147764745409a84039a5b83a958333e
Bartosz Maleta <bartosz.maleta@gmail.com>
2019-12-02 21:29:21 +0100

    Add Description

--- katas_codewars/sort_and_star.py
+++ katas_codewars/sort_and_star.py
@@ -1,3 +1,9 @@
+# You will be given an vector of string(s). You must sort it alphabetically (case-sensitive!!) and then return the first value.
+
+# The returned value must be a string, and have "***" between each of its letters.
+
+# You should not remove or add elements from/to the array.
+
 array = ["bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps"]
 
 # print(array)
@@ -20,8 +26,7 @@ def two_sort(array):
     print(result)
     len_ = len(result)
     true_len = len_ - 3
-    list_help = []
-    list_help.append(result)
+    result = result[:true_len]
     print(result[:true_len])
     return result
 
